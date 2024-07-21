fa83074cf721954cf589354c0cf46b6ab0515c6e
Amyat103 <zhoudavid7312@gmail.com>
2024-07-21 22:08:21 +0700

    Updated Redis configuration

--- config/settings.py
+++ config/settings.py
@@ -179,7 +179,7 @@ CORS_ALLOWED_ORIGINS = [
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": os.environ.get("REDIS_URL", "redis://localhost:6379/1"),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
