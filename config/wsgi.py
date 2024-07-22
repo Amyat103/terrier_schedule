d01c90bcfefcde285ad7bd988b6dcadc2c5f222e
Amyat103 <zhoudavid7312@gmail.com>
2024-07-22 14:01:16 +0700

    fix wsgi file

--- config/wsgi.py
+++ config/wsgi.py
@@ -12,8 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
 application = get_wsgi_application()
 application = WhiteNoise(application)
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-application = get_wsgi_application()
