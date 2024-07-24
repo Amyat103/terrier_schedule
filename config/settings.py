6df69601717129aee25a5f9a10c1f5e0925fa1a7
Amyat103 <zhoudavid7312@gmail.com>
2024-07-24 17:18:33 +0700

    Revert "add method to block unauthorized access"
    This reverts commit 21a7d819f398c00d3902d1613cb71bd870b6ab2f.

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 
 import dj_database_url
 import environ
-from django.core.management.utils import get_random_secret_key
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -56,7 +55,6 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
-    "config.auth_middleware.APIAuthMiddleware",
     "config.referer_middleware.RefererCheckMiddleware",
     "django.middleware.gzip.GZipMiddleware",
     "django.middleware.security.SecurityMiddleware",
@@ -180,7 +178,6 @@ LOGGING = {
 }
 
 CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
     "https://terrier-schedule.dev",
@@ -235,6 +232,3 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "zhoudavid7312@gmail.com")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-
-
-API_SECRET_KEY = os.environ.get("API_SECRET_KEY")
--- frontend/src/api/course_fetch.js
+++ frontend/src/api/course_fetch.js
@@ -7,15 +7,9 @@ const API_URL =
 
 let memoryStorage = {};
 
-const API_SECRET_KEY = import.meta.env.VITE_API_SECRET_KEY;
-
 const getDataVersion = async () => {
   try {
-    const response = await axios.get(`${API_URL}/data-version/`, {
-      headers: {
-        Authorization: `Bearer ${API_SECRET_KEY}`,
-      },
-    });
+    const response = await axios.get(`${API_URL}/data-version/`);
     console.log('Data version response:', response.data);
     return response.data.version;
   } catch (error) {
@@ -81,11 +75,7 @@ export const fetchCourses = async () => {
 
     if (cachedVersion !== serverVersion) {
       console.log('Fetching fresh course data from server');
-      const response = await axios.get(`${API_URL}/courses/`, {
-        headers: {
-          Authorization: `Bearer ${API_SECRET_KEY}`,
-        },
-      });
+      const response = await axios.get(`${API_URL}/courses/`);
       console.log('Courses API Response:', response);
       const success = storeData('coursesData', response.data);
       if (success) {
@@ -124,11 +114,7 @@ export const fetchSections = async () => {
 
     if (cachedVersion !== serverVersion) {
       console.log('Fetching fresh section data from server');
-      const response = await axios.get(`${API_URL}/sections/`, {
-        headers: {
-          Authorization: `Bearer ${API_SECRET_KEY}`,
-        },
-      });
+      const response = await axios.get(`${API_URL}/sections/`);
       console.log('Sections API Response:', response);
       if (!Array.isArray(response.data)) {
         console.error('Unexpected data format for sections:', response.data);
